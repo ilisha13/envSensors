19d11f9a51ef64414bc4a28720b8d6831f90b786
Hari Ramachandran <hari_ramachandran1@hotmail.com>
2016-07-09 14:11:11 +0800

    Commented the code a little

--- pythonPC/UDPReceive.py
+++ pythonPC/UDPReceive.py
@@ -1,9 +1,20 @@
-import socket
-UDP_IP = '10.0.0.152'
-UDP_PORT = 10000
+# Sample code to setup a local UDP listener or a specific port, and then
+# display a response to any received data.
 
+import socket
+MY_IP = '10.0.0.152' #add your IP address or change to local host
+MY_PORT = 10000
+
+# Open the socket and set it to UDP
 sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM) # UDP
-sock.bind((UDP_IP, UDP_PORT))
+
+# Bind the socket. There is not error checking, so if there is no
+# IP address this code will puke on you. We should add some exception
+# handling code here.
+
+sock.bind((MY_IP, MY_PORT))
+
+# Now go into a while lop, waiting for a response 
 
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
