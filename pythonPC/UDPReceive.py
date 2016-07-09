3515bbf5cdbe41f09366bde6b758117e554ea76f
Hari Ramachandran <hari_ramachandran1@hotmail.com>
2016-07-09 17:40:41 +0800

    Edited this. Added prompt. Tested with UDPReceive, but not tested against Edison. Note, need to add npm prompt before you test this out.

--- pythonPC/UDPReceive.py
+++ pythonPC/UDPReceive.py
@@ -2,17 +2,25 @@
 # display a response to any received data.
 
 import socket
-MY_IP = '10.0.0.152' #add your IP address or change to local host
-MY_PORT = 10000
+
+print ('-----------------------------------------------')
+print ('|           Python UDP Listener                |')
+print ('-----------------------------------------------')            
 
 # Open the socket and set it to UDP
 sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM) # UDP
+ipAddress = input('Please enter your IP address: ')
+portNum = input('Please enter the port number you want to listen on: ')
 
-# Bind the socket. There is not error checking, so if there is no
+# Bind the socket. There is no error checking, so if there is no
 # IP address this code will puke on you. We should add some exception
 # handling code here.
 
-sock.bind((MY_IP, MY_PORT))
+#sock.bind((MY_IP, MY_PORT))
+sock.bind((ipAddress, int(portNum)))
+
+
+print ("Waiting for message ...")
 
 # Now go into a while lop, waiting for a response 
 
--- sensorNode/nodeUDPSend.js
+++ sensorNode/nodeUDPSend.js
@@ -1,7 +1,47 @@
+
 var dgram = require('dgram');
-var responseBuffer = new Buffer( "THIS IS A MESSAGE FROM THE EDISON" );
+var util = require('util');
+var prompt = require('prompt'); /* NPM Module to handle keyboard input. npm install prompt */
+ 
+/* Setup the Socket for UDP and set up the message buffer as well */
 var client = dgram.createSocket('udp4');
-client.send(responseBuffer, 0, responseBuffer.length, 10000, '10.0.0.152', (err) => {
-	console.log("Sent message")
-  client.close();
-});
+
+console.log('-----------------------------------------------');
+console.log('|           Node  UDP Sender                  |');
+console.log('-----------------------------------------------');          
+
+/* Get user input */
+prompt.start();
+
+prompt.get(['ipAddress', 'portNum', 'message'], function (err, result) {
+    if (err) { return onPromptErr(err); }
+    console.log('ipAddress: ' + result.ipAddress);
+    console.log('portNum  : ' + result.portNum);
+    console.log('message  " ' + result.message);
+    console.log('message length = ' + result.message.length);
+    
+    /* Now send the message out */
+
+    client.send(result.message, 0, result.message.length , result.portNum, result.ipAddress, function (err, data) {
+        if (err) {
+            /* We got a send error */
+            console.log("Error = ", err);
+            client.close();
+        }
+        else {
+            /* Successfully sent! */
+            console.log('Message sent to ', result.ipAddress, ' on port # ', result.portNum);
+            client.close();
+        }
+    });
+}); 
+
+/********************  Functions **************************/
+
+function onPromptErr(err) {
+    console.log(err);
+    return 1;
+}
+
+
+ 
